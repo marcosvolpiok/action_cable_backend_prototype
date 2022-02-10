ff95aea2e9c2b32eb11be181f5cdd5899e7c2bb3
Marcos Volpi <marcos@marcosvolpi.com>
2022-02-10 05:58:51 -0300

    Broadcasting from controller

--- app/controllers/chat_controller.rb
+++ app/controllers/chat_controller.rb
@@ -1,5 +1,6 @@
 class ChatController < ApplicationController
     def index
+        ActionCable.server.broadcast("chat_Best Room", {sent_by: "marcos", body: "hi!"})
         render 'chat/index'
     end
 end
